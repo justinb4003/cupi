bae2b7a14145d0c981345fa0bdc184abc3ed6ef6
Justin Buist <justin.buist@gmail.com>
2018-03-29 21:02:26 -0400

    Start of networking code.

--- cuspeak.py
+++ cuspeak.py
@@ -3,19 +3,31 @@
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-import RPi.GPIO as GPIO
+RUN_ON_PI = False
+
+SOUND_DIR="/home/jbuist/git/cupi/"
+
+if RUN_ON_PI:
+    import RPi.GPIO as GPIO
 from pygame import mixer
+from threading import Thread
 import pygame
+import socket
+import time
 
-GPIO.setmode(GPIO.BCM)
-
-# Appearntly not needed for IO3.. go fig.
-#GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(3, GPIO.IN)
-GPIO.setup(5, GPIO.OUT)
+if RUN_ON_PI:
+    GPIO.setmode(GPIO.BCM)
+    # Appearntly not needed for IO3.. go fig.
+    #GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(3, GPIO.IN)
+    GPIO.setup(5, GPIO.OUT)
 
 def getSpeakButton():
-    status = GPIO.input(3)
+    if RUN_ON_PI:
+        status = GPIO.input(3)
+    else:
+        status = 1
+
     if status == 1:
         status = 0
     elif status == 0:
@@ -28,18 +40,38 @@ def playSoundFile(filename):
     while mixer.music.get_busy():
         pygame.time.Clock().tick(10)
 
+def getNetCommand():
+    global keepRunning
+    while keepRunning:
+        c, addr = s.accept()
+        print 'Connection from:', addr
+        retVal = c.recv(1024)
+        print 'received:', retVal
+        c.close()
+
+
+keepRunning = True
+
+s = socket.socket()
+s.bind(('', 2048))
+s.listen(5) # find out why 5... 
+sthread = Thread(name="server", target=getNetCommand)
+sthread.setDaemon(True)
+sthread.start()
 
 # Startup
 pygame.init()
 mixer.init()
-playSoundFile("/opt/cside/bin/bootup.mp3")
+playSoundFile(SOUND_DIR + "bootup.mp3")
 
-while True:
-    GPIO.output(5, 0)
+while keepRunning:
+    if RUN_ON_PI:
+        GPIO.output(5, 0)
     status = getSpeakButton()
     #print status
     if status == 1:
-        GPIO.output(5, 1)
+        if RUN_ON_PI:
+            GPIO.output(5, 1)
         playSoundFile("/opt/cside/bin/lawman_nocash_green_egg.mp3")
 
 
